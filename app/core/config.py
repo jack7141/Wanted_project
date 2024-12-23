da3191322e90a7a2bb24c3ec934c10f43cfc7b66
황광회 <jack7141@nate.com>
2024-12-23 13:46:43 +0900

    Setting Init

--- app/core/config.py
+++ app/core/config.py
@@ -15,15 +15,15 @@ class Configs(BaseSettings):
     API: str = "/api"
     API_V1_STR: str = "/api/v1"
     API_V2_STR: str = "/api/v2"
-    PROJECT_NAME: str = "fca-api"
+    PROJECT_NAME: str = "wanted-api"
     ENV_DATABASE_MAPPER: dict = {
         "prod": "fca",
-        "stage": "stage-fca",
-        "dev": "dev-fca",
-        "test": "test-fca",
+        "stage": "stage-wanted",
+        "dev": "dev-wanted",
+        "test": "test-wanted",
     }
     DB_ENGINE_MAPPER: dict = {
-        "postgresql": "postgresql",
+        "postgresql": "postgresql+psycopg2",
         "mysql": "mysql+pymysql",
     }
 
@@ -33,19 +33,16 @@ class Configs(BaseSettings):
     DATETIME_FORMAT: str = "%Y-%m-%dT%H:%M:%S"
     DATE_FORMAT: str = "%Y-%m-%d"
 
-    # auth
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 30  # 60 minutes * 24 hours * 30 days = 30 days
 
     # CORS
     BACKEND_CORS_ORIGINS: List[str] = ["*"]
 
     # database
     DB: str = os.getenv("DB", "postgresql")
-    DB_USER: str = os.getenv("DB_USER")
-    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
-    DB_HOST: str = os.getenv("DB_HOST")
-    DB_PORT: str = os.getenv("DB_PORT", "3306")
+    DB_USER: str = os.getenv("DB_USER", "postgres")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "password")
+    DB_HOST: str = os.getenv("DB_HOST", "localhost")
+    DB_PORT: str = os.getenv("DB_PORT", "5432")
     DB_ENGINE: str = DB_ENGINE_MAPPER.get(DB, "postgresql")
 
     DATABASE_URI_FORMAT: str = "{db_engine}://{user}:{password}@{host}:{port}/{database}"
